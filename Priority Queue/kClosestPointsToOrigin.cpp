ee3e5e05e14874aaa05766ab2498f23f3963c11b
nileshnj993 <nileshnj993@gmail.com>
2021-08-15 01:15:45 +0530

    minor updates

--- Priority Queue/kClosestPointsToOrigin.cpp
+++ Priority Queue/kClosestPointsToOrigin.cpp
@@ -8,8 +8,9 @@ vector<pair<double,double>> kClosest(vector<pair<double,double>> v, double k){
     priority_queue<ppi> maxH; // max heap to pop max distance points
     double distance;
     for(double i=0;i<v.size();i++){
-        distance = sqrt(pow(v[i].first,2) + pow(v[i].second,2)); // first element of max Heap element pair = euclidian distance from origin
-        maxH.push(make_pair(distance,v[i])); // making a pair of distance and the coordinates
+        // distance = sqrt(pow(v[i].first,2) + pow(v[i].second,2)); // first element of max Heap element pair = euclidian distance from origin
+        distance = pow(v[i].first,2) + pow(v[i].second,2); // sqrt not needed as we can square on all sides
+        maxH.push(make_pair(distance,v[i])); // making a pair of distance and the coordinates. we can make a pair of distance and the index as well to save space
         if(maxH.size()>k){
             maxH.pop();
         }
