d8037b9488d09aeb2bcf0687a286b98a1f24bb71
nileshnj993 <nileshnj993@gmail.com>
2021-08-28 10:13:09 +0530

    added possible strings backtracking code

---
+++ Miscellaneous/possibleStrings.cpp
@@ -0,0 +1,36 @@
+// find all possible strings that can be formed by replacing ? with either 1 or 0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> res;
+
+void backtrack(string s,int i){
+    if(i==s.size()){
+        res.push_back(s);
+        return;
+    }
+    if(s[i]=='?'){
+        s[i] = '0';
+        backtrack(s,i+1);
+        s[i] = '1';
+        backtrack(s,i+1);
+        // s[i] = '?';
+    }
+    else{
+        backtrack(s,i+1);
+    }
+}
+  
+    
+
+int main(){
+    int i = 0;
+    string s = "11??1";
+    string temp="";
+    int n = s.size();
+    backtrack(s,i);
+    for(auto i : res){
+        cout<<i<<"\n";
+    }
+}
